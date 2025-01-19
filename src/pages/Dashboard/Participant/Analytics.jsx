f2c7225d29c8de01f147805b719f159a6b61aac2
imran. <fozlerabbi.me@gmail.com>
2025-01-19 22:21:54 +0600

    Analytics DONE

--- src/pages/Dashboard/Participant/Analytics.jsx
+++ src/pages/Dashboard/Participant/Analytics.jsx
@@ -2,16 +2,29 @@ import React from 'react';
 import Heading from '../../../components/Shared/Heading';
 import CampAnalytics from '../../../components/Dashboard/Analytics/CampAnalytics';
 import { useQuery } from '@tanstack/react-query';
+import useAxiosPublic from '../../../hooks/usePublic';
+import useAuth from '../../../hooks/useAuth';
+import LoadingSpinner from '../../../components/Shared/LoadingSpinner';
+
 
 const Analytics = () => {
-    const { data: camps } = useQuery({
-        queryKey: 'camps',
+    const axiosPublic = useAxiosPublic();
+    const { user, loading } = useAuth();
+    const { data: registeredCamps = [], isLoading, refetch } = useQuery({
+        queryKey: ['registeredCamps', user?.email],
+        enabled: !loading,
+        queryFn: async () => {
+            const { data } = await axiosPublic.get(`/registered-camps/${user?.email}`);
+            return data;
+        }
     });
 
+    if (isLoading) return <LoadingSpinner />;
+    console.log(registeredCamps);
     return (
         <div>
             <Heading title='Analytics' subtitle='View your analytics here' center={true} />
-            <CampAnalytics />
+            <CampAnalytics camps={registeredCamps} />
         </div>
     );
 };
